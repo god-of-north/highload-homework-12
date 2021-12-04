ff74040f36160fc107adda1690c3d2e332c3bb7d
Maksym Lesyk <mnl@industrialinnovationgroup.com>
2021-12-04 12:23:52 +0200

    publisher and consumer done

--- services/web/consumer.py
+++ services/web/consumer.py
@@ -1,7 +1,14 @@
 import signal
 from time import sleep
+import threading
 
 import redis
+import greenstalk
+
+
+redis_rdb = redis.Redis(host='redis-rdb', port=6379)
+redis_aof = redis.Redis(host='redis-aof', port=6379)
+beanstalkd = greenstalk.Client(('beanstalkd', 11300))
 
 
 class GracefulKiller:
@@ -13,10 +20,54 @@ class GracefulKiller:
     def exit_gracefully(self, *args):
         self.kill_now = True
 
-def consume_worker():
+
+def consume_beanstalkd():
+    date = None
+    id = None
+
+    job = beanstalkd.reserve()
+    if job is not None:
+        date, id, _ = job.body.split('|')
+        beanstalkd.delete(job)
+    return date, id
+
+
+def consume_rdb():
+    date = None
+    id = None
+
+    data = redis_rdb.rpop('REDIS_RDB_QUEUE')
+    if data is not None:
+        date, id, _ = data.decode("utf-8") .split('|')
+    return date, id
+
+def consume_aof():
+    date = None
+    id = None
+
+    data = redis_aof.rpop('REDIS_AOF_QUEUE')
+    if data is not None:
+        date, id, _ = data.decode("utf-8").split('|')
+    return date, id
+
+def try_print(date, id):
+    if date!=None and id!=None:
+        print("{: >20} {: >10}".format(date, id), flush=True)
+
+def consume_worker(consume_func):
+    while True:
+        try_print(*consume_func())
+
+if __name__ == '__main__':
+    r1 = threading.Thread(name='consume_beanstalkd', daemon=True, target=lambda: consume_worker(consume_beanstalkd))
+    r2 = threading.Thread(name='consume_rdb', daemon=True, target=lambda: consume_worker(consume_rdb))
+    r3 = threading.Thread(name='consume_aof', daemon=True, target=lambda: consume_worker(consume_aof))
+
+    r1.start()
+    r2.start()
+    r3.start()
+
     killer = GracefulKiller()
     while not killer.kill_now:
         sleep(1)
-        print('Consumer alive', flush=True)
 
-consume_worker()
--- services/web/producer.py
+++ services/web/producer.py
@@ -1,14 +1,54 @@
-import os
-import  json
-from time import sleep
+from random import choice
+from datetime import datetime
+import string
 
 import redis
-from flask import Flask, request
+import greenstalk
+from flask import Flask
 
 
 app = Flask(__name__)
 
+redis_rdb = redis.Redis(host='redis-rdb', port=6379)
+redis_aof = redis.Redis(host='redis-aof', port=6379)
+beanstalkd = greenstalk.Client(('beanstalkd', 11300))
 
-@app.route("/")
-def index():
-    return 'Test'
+beanstalkd_id = 0
+redis_rdb_id = 0
+redis_aof_id = 0
+
+
+def generate_message(id, size):
+    ret = f'{datetime.now()}|{id}|'
+    size = size - len(ret)
+    return ret  + ''.join(choice(string.digits+string.ascii_letters) for _ in range(size - len(ret)))
+
+@app.route("/publish_redis_rdb")
+def publish_redis_rdb():
+    global redis_rdb_id
+
+    message = generate_message(redis_rdb_id, 1024)
+    redis_rdb.rpush('REDIS_RDB_QUEUE', message)
+    redis_rdb_id += 1
+
+    return message
+
+@app.route("/publish_redis_aof")
+def publish_redis_aof():
+    global redis_aof_id
+
+    message = generate_message(redis_aof_id, 1024)
+    redis_aof.rpush('REDIS_AOF_QUEUE', message)
+    redis_aof_id += 1
+
+    return message
+
+@app.route("/publish_beanstalkd")
+def publish_beanstalkd():
+    global beanstalkd_id
+
+    message = generate_message(beanstalkd_id, 1024)
+    beanstalkd.put(message)
+    beanstalkd_id += 1
+
+    return message
