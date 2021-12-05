fbe4bb7abda80a836ef31615197c1c4adfd144f3
Maksym Lesyk <mnl@industrialinnovationgroup.com>
2021-12-05 11:20:35 +0200

    batch mode added

--- services/web/producer.py
+++ services/web/producer.py
@@ -4,7 +4,7 @@ import string
 
 import redis
 import greenstalk
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -23,32 +23,60 @@ def generate_message(id, size):
     size = size - len(ret)
     return ret  + ''.join(choice(string.digits+string.ascii_letters) for _ in range(size - len(ret)))
 
+def get_params():
+    size = request.args.get('size') # message size
+    batch = request.args.get('batch') # batch size
+    return int(size), int(batch)
+
 @app.route("/publish_redis_rdb")
 def publish_redis_rdb():
     global redis_rdb_id
 
-    message = generate_message(redis_rdb_id, 1024)
-    redis_rdb.rpush('REDIS_RDB_QUEUE', message)
-    redis_rdb_id += 1
+    size, batch = get_params()
 
-    return message
+    if batch == 1:
+        message = generate_message(redis_rdb_id, size)
+        redis_rdb.rpush('REDIS_RDB_QUEUE', message)
+        redis_rdb_id += 1
+    else:
+        p = redis_aof.pipeline()
+        for _ in range(batch):
+            message = generate_message(redis_rdb_id, size)
+            redis_rdb.rpush('REDIS_RDB_QUEUE', message)
+            redis_rdb_id += 1
+        p.execute()
+
+    return 'OK'
 
 @app.route("/publish_redis_aof")
 def publish_redis_aof():
     global redis_aof_id
 
-    message = generate_message(redis_aof_id, 1024)
-    redis_aof.rpush('REDIS_AOF_QUEUE', message)
-    redis_aof_id += 1
+    size, batch = get_params()
 
-    return message
+    if batch == 1:
+        message = generate_message(redis_aof_id, size)
+        redis_aof.rpush('REDIS_AOF_QUEUE', message)
+        redis_aof_id += 1
+    else:
+        p = redis_aof.pipeline()
+        for _ in range(batch):
+            message = generate_message(redis_aof_id, size)
+            redis_aof.rpush('REDIS_AOF_QUEUE', message)
+            redis_aof_id += 1
+        p.execute()
+
+    return 'OK'
 
 @app.route("/publish_beanstalkd")
 def publish_beanstalkd():
     global beanstalkd_id
 
-    message = generate_message(beanstalkd_id, 1024)
-    beanstalkd.put(message)
-    beanstalkd_id += 1
+    size, batch = get_params()
 
-    return message
+    for _ in range(batch):
+        message = generate_message(beanstalkd_id, size)
+        beanstalkd.put(message)
+        beanstalkd_id += 1
+
+    return 'OK'
